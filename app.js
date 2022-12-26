93276c76e004eb9f8d8a6328afed4d43ff9e67af
abbie930 <abbiecheng930@gmail.com>
2022-12-26 18:57:28 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = 3000
+
+mongoose.set('strictQuery', true)
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('this is a web')
