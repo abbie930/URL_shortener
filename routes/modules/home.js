25f5f8a0a4b7cfea6049e532f05b35c6227f3edc
abbie930 <abbiecheng930@gmail.com>
2022-12-29 17:06:08 +0800

    refactor: use async-await in router.post

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -13,48 +13,46 @@ router.get('/', (req, res) => {
 router.post('/', async (req, res) => {
   const baseURL = req.headers.origin
   const originalURL = req.body.originalUrl
+ 
+  try {
+    const shortURL = shortenURL()
 
-  //check URL if valid
-  if (!originalURL) return res.redirect('/')
-  //if valid, create the shortURL code
-  const shortURL = shortenURL()
+    let data // should be either the one found in database or the one gets created
 
-  URL.findOne({ originalURL })
-    .then(data => 
-      data ? data : URL.create({ shortURL, originalURL })
-    )
-    .then(data => 
-      res.render("index", {
-        origin: baseURL,
-        shortURL: data.shortURL,
-      })
-    )
-    .catch(error => console.log(error))
+    data = await URL.findOne({ originalURL })
+
+    if(!data) {
+      data = await URL.create({ shortURL, originalURL})
+    }
+
+    res.render('index', {
+      origin: baseURL,
+      shortURL: data.shortURL
+    })
+  } catch(error) {
+    console.log(error)
+  }
 })
 
 
 //redirect to the original URL
 router.get('/:shortURL', async (req, res) => {
-  try{
-      const { shortURL } = req.params
-      //find a document match to the code in req.params.shortURL
-      const data = await URL.findOne({ shortURL })
-      //if null
-      if(!data) {
-        console.log(req.headers)
-        return res.render('error', {
-          errorURL: "http://" + req.headers.host + "/" + shortURL
-        })
-      }
-      //else: redirect the user to original link
-      res.redirect(data.originalURL)
-    } catch (error) {
-      console.log(error)
+  try {
+    const { shortURL } = req.params
+    //find a document match to the code in req.params.shortURL
+    const data = await URL.findOne({ shortURL })
+    //if null
+    if(!data) {
+      return res.render('error', {
+        errorURL: "http://" + req.headers.host + "/" + shortURL
+      })
     }
-  })
-
-
-
+    //else: redirect the user to original link
+    res.redirect(data.originalURL)
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 
 module.exports = router
