053597007f424fb423dfd2bfdb6046c0342b82b4
abbie930 <abbiecheng930@gmail.com>
2022-12-29 14:09:05 +0800

    feat: add route to redirect the shortURL to originalURL

--- app.js
+++ app.js
@@ -34,29 +34,61 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-
-app.post('/', (req, res) => {
+//create the post route
+app.post('/', async (req, res) => {
   const baseURL = req.headers.origin
   const originalURL = req.body.originalUrl
+
+  //check URL if valid
+  if (!originalURL) return res.redirect('/')
+  //if valid, create the shortURL code
   const shortURL = shortenURL()
 
-  if (!originalURL) return res.redirect('/')
+  try {
+      const data = await URL.findOne({ originalURL })
 
-  URL.findOne({ originalURL })
-    .then(data => 
-      // console.log(data)
-      data ? data : URL.create({ shortURL, originalURL })
-    )
-    .then(data =>
-      // console.log(data)
+      if(!data) {
+        await URL.create({ shortURL, originalURL })
+      }
       res.render('index', {
-        origin: baseURL,
+        origin: baseURL, 
         shortURL: data.shortURL,
       })
-    )
-    .catch(error => console.log(error))
+    } catch (error) {
+    console.log(error)
+    }
+    // .then(data => 
+    //   data ? data : URL.create({ shortURL, originalURL })
+    // )
+    // .then(data =>
+    //   res.render('index', {
+    //     origin: baseURL,
+    //     shortURL: data.shortURL,
+    //   })
+    // )
+    // .catch(error => console.log(error))
 })
 
+
+//redirect to the original URL
+app.get('/:shortURL', async (req, res) => {
+  try{
+      const { shortURL } = req.params
+      //find a document match to the code in req.params.shortURL
+      const data = await URL.findOne({ shortURL })
+      //if null
+      if(!data) {
+        console.log(req.headers)
+        return res.render('error', {
+          errorURL: "http://" + req.headers.host + "/" + shortURL
+        })
+      }
+      //else: redirect the user to original link
+      res.redirect(data.originalURL)
+    } catch (error) {
+      console.log(error)
+    }
+  })
 
 
 app.listen(port, () => {
