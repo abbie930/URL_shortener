1680945eb7bb41de993ecd64321b2ddd0bae895a
abbie930 <abbiecheng930@gmail.com>
2022-12-29 15:10:49 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,36 +1,19 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const URL = require('./models/URL')
-const shortenURL = require('./utils/shortenURL')
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
 
-mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGODB_URI)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
-
 
 
 app.listen(port, () => {
